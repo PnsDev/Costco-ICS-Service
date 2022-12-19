a6c24ac9cb8970edc85d3b8e42b5aae27785d746
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-12-19 17:13:08 -0600

    Removed un-used functions

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -23,39 +23,6 @@ export function dateDif(date1: Date, date2: Date): dateDiff {
 }
 
 /**
- * Converts a date to a specific timezone
- * @param date The date to convert
- * @param tzString The timezone to convert to
- * @returns A date with the specified timezone
- */
-export function convertTZ(date: Date, tzString: string) {
-    return new Date(date.toLocaleString("en-US", {timeZone: tzString}));   
-}
-
-/**
- * Creates a date with a specific timezone (UTC + offset)
- * @param timeZone The timezone to use
- * @param year The year
- * @param month The month
- * @param day The day
- * @param hour The hour
- * @param minute The minute
- * @param second The second
- * @returns A date with the specified timezone
- */
-export function dateWithTimeZone(timeZone, year, month, day, hour, minute, second) {
-    let date = new Date(Date.UTC(year, month, day, hour, minute, second));
-  
-    let utcDate = new Date(date.toLocaleString('en-US', { timeZone: "UTC" }));
-    let tzDate = new Date(date.toLocaleString('en-US', { timeZone: timeZone }));
-    let offset = utcDate.getTime() - tzDate.getTime();
-  
-    date.setTime( date.getTime() + offset );
-  
-    return date;
-};
-
-/**
  * Converts a 12 hour time to a 24 hour time
  * @param time12h The 12 hour time
  * @returns The 24 hour time
@@ -76,4 +43,14 @@ export function convertTime12to24(time12h: string): string {
  */
 export function numToWeekDate(num: number): string {
     return weekDays[num - 1];
+}
+
+/**
+ * Checks if two dates are equal
+ * @param date1 The first date
+ * @param date2 The second date
+ * @returns True if the dates are equal, false otherwise
+ */
+export function equalDates(date1: Date, date2: Date): boolean {
+    return date1.getTime() === date2.getTime();
 }
