1420d2e01736260f8f010427a1263676c514f00b
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-12-19 21:43:10 -0600

    Added jobs to start

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import EventHolder from "./classes/EventHolder";
 import ICSServer from "./classes/ICSServer";
 import Scheduler from "./scheduler/Scheduler";
 import path from "path";
+import CalendarEvent from './classes/CalendarEvent';
 
 const eventHolder: EventHolder = new EventHolder();
 let icsServer: ICSServer;
 let mongooseDb: mongoose.Mongoose;
 let scheduler: Scheduler;
 
+let failedAttempts = 0;
+
 
 async function startServer() {
+    mongoose.set('strictQuery', true);
     mongooseDb = await mongoose.connect(`${process.env.MONGODB_URI}`);
+
     console.log(`Connected to database!`);
 
 
@@ -24,10 +29,39 @@ async function startServer() {
         {
             name: "fetchSchedule",
             path: path.join(__dirname, "jobs", "fetchSchedule.ts"),
-			endBehavior: (job, res) => {
-				console.log(res);
-			}
+            repeatEvery: 21600000,
+            errorBehavior: (job) => {
+                if (++failedAttempts > 7) {
+                    console.log("Too many failed attempts. Shutting down.");
+                    process.exit(1);
+                }
+                // If the job fails, we want to try again in 2 hours
+                scheduler.scheduleJob({...job, repeatEvery: null, initialDelay: 7200000});
+            },
+            endBehavior: (job, res) => {
+                failedAttempts = 0;
+                scheduler.scheduleJob({
+                    name: "dbUpdate",
+                    path: path.join(__dirname, "jobs", "dbUpdate.ts"),
+                    jobVariable: res,
+                    endBehavior: () => {
+                        scheduler.runJobNow("dbFetch");
+                    }
+                });
+            }
         },
+        {
+            name: "dbFetch",
+            path: path.join(__dirname, "jobs", "dbFetch.ts"),
+            initialDelay: 1,
+            repeatEvery: 43200000,
+            jobVariable: { 
+                eventHolder: eventHolder, 
+                dataFrom: new Date((new Date().getTime() - 2.628e+9)) // One month ago
+            },
+            endBehavior: () => {icsServer.startServer()} // Start the server in case it hasn't been started yet (since we don't want to return empty calendars)
+        }
+        // TODO: Maybe it would make more sense to just pull data when requested???
     ]);
 }
 
