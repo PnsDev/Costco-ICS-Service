7dbb889fe2b5046d14deea0df47f24a73e0556f1
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-12-24 16:02:17 -0600

    Documented event holder class

--- src/classes/EventHolder.ts
+++ src/classes/EventHolder.ts
@@ -4,21 +4,36 @@ import CalendarEvent from "./CalendarEvent";
  * Holds all the events for 
  */
 export default class EventHolder {
-    events: Array<CalendarEvent> = [];
+    #events: Array<CalendarEvent> = [];
 
-    addEvent(event: CalendarEvent) {
-        this.events.push(event);
+    /**
+     * Adds an event to the list of events
+     * @param event The event to add
+     */
+    public addEvent(event: CalendarEvent) {
+        this.#events.push(event);
+    }
+
+    /**
+     * Replaces the current list of events with a new list
+     * @param events The list of events to replace the current list with
+     */
+    public setEvents(events: Array<CalendarEvent>) {
+        this.#events = events;
     }
     
-    clearEvents() {
-        this.events = [];
+    /**
+     * Clears all the events from the list
+     */
+    public clearEvents() {
+        this.#events.length = 0
     }
 
+    /**
+     * Turns all the events into ICSEvents
+     * @returns The list of events
+     */
     turnIntoICSEvents() {
-        let icsEvents: Array<any> = [];
-        for (let e of this.events) {
-            icsEvents.push(e.turnIntoICSEvent());
-        }
-        return icsEvents;
+        return this.#events.map(e => e.turnIntoICSEvent());
     }
 }
--- src/jobs/dbFetch.ts
+++ src/jobs/dbFetch.ts
@@ -1,14 +1,11 @@
-import CalendarEvent from "../classes/CalendarEvent";
-import EventHolder from "../classes/EventHolder"
+import EventHolder from "../classes/EventHolder";
 import scheduledDate from "../schemas/scheduledDate";
 import { eventsFromDBArray } from "../utils/miscUtils";
 
 export default async function job(jobData: dbFetchData) {
-    // This is a very simple job that just fetches data from the database
-    jobData.eventHolder.events = eventsFromDBArray(await scheduledDate.find({startTime: {$gte: jobData.dataFrom}}));
+    // This is a very simple job that just fetches data from the database and passes it to the eventHolder
+    jobData.eventHolder.setEvents(eventsFromDBArray(await scheduledDate.find({startTime: {$gte: jobData.dataFrom}})));
 }
-
-
 
 type dbFetchData = {
     eventHolder: EventHolder,
