2d1ac355bcd908f34cd3797f5e0f682c79398fc9
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-12-23 18:03:32 -0600

    Await for the agent to be changed

--- src/jobs/fetchSchedule.ts
+++ src/jobs/fetchSchedule.ts
@@ -19,7 +19,7 @@ export default async function job() {
         args: ['--no-sandbox']
     });
     const page: Puppeteer.Page = await browser.newPage();
-    page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
     await page.goto('https://ess.costco.com/', {waitUntil : 'networkidle2' });
 
     /**
