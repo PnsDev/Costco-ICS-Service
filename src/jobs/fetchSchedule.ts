b2267ed5bd14de4d6281a89c0bc59f77f432cf21
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-12-24 00:09:21 -0600

    Removed unused args and turned into headless

--- src/jobs/fetchSchedule.ts
+++ src/jobs/fetchSchedule.ts
@@ -15,14 +15,9 @@ export default async function job() {
     const finalDates: CalendarEvent[] = [];
 
     const browser: Puppeteer.Browser = await Puppeteer.launch({
-        headless: false,
+        headless: true,
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-infobars',
-            '--window-position=0,0',
-            '--ignore-certifcate-errors',
-            '--ignore-certifcate-errors-spki-list',
             '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
         ]
     });
